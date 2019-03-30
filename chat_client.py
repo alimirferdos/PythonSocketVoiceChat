abc5cc125c3f9f9851a9c0452f5078544a6b2ed5
mammad2m <48725555+mammad2m@users.noreply.github.com>
2019-03-30 21:54:59 +0430

    Add files via upload

--- chat_client.py
+++ chat_client.py
@@ -19,16 +19,19 @@ class ChatClient:
 
         self.BUFFSIZE = 1024
         self.ADDR = (self.HOST, self.PORT)
-
+        self.request_sent = False
+        self.you_have_req = False
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        self.sender = ""
     def run(self):
         self.client_socket.connect(self.ADDR)
         print("Connected!")
         receive_thread = Thread(target=self.receive)
         receive_thread.start()
-        self.send(input())
-        self.get_client_list()
+        self.send(input()) #send username to server
+        commands_thread = Thread(target=self.get_command)
+        commands_thread.start()
+        #self.get_client_list()
 
     def receive(self):
         """Handles receiving of messages."""
@@ -37,7 +40,14 @@ class ChatClient:
                 msg = self.client_socket.recv(self.BUFFSIZE)
                 print(msg.decode("utf8"))
             except UnicodeDecodeError:
-                print(pickle.loads(msg))
+                msg = pickle.loads(msg)
+                if(msg['command'] == 'send_file') :
+                    if(self.you_have_req == False):
+                        self.you_have_req = True
+                        self.sender = msg['sender']
+                        print(msg['sender'], 'wants send a file to you! Do you accept?! (yes/no)\n')
+                    else:
+                        print(msg['sender'], 'wants send a file to you! but you already have an unanswerd request')
             except OSError:  # Possibly client has left the chat.
                 break
 
@@ -47,24 +57,48 @@ class ChatClient:
             self.client_socket.send(bytes(msg, "utf8"))
         else:
             self.client_socket.send(msg)
-            if msg == pickle.dumps({"command": "quite"}):
-                self.client_socket.close()
-                print("msg sent")
+            #if msg == pickle.dumps({"command": "quite"}):
+                #self.client_socket.close()
+                #print("msg sent")
 
-    @atexit.register
-    def on_closing(self):
+    #@atexit.register
+    def disconnect(self):
         """This function is to be called when the window is closed."""
-        self.send(pickle.dumps({"command": "quite"}))
+        self.send(pickle.dumps({"command": "disconnect"}))
+        self.client_socket.close()
+        print('disconnected')
+    def get_command(self):
+        while True:
+            if(self.request_sent):
+                print('waiting for request answer!\n')
+                while self.request_sent:
+                    pass
+            else:
+                command = input('type command (send_file, disconnect, get_clients) : \n')
+                if(command == 'send_file'):
+                    receiver = input('type username of receiver : \n')
+                    self.send_request(receiver)
+                elif(command == 'disconnect'):
+                    self.disconnect()
+                    break
+                elif (command == 'get_clients'):
+                    self.get_client_list()
+                elif (command == 'yes' or command =='no') and self.you_have_req:
+                    self.send(pickle.dumps({"command": "answer", "value": command, 'sender': self.sender}))
+                    self.you_have_req = False
+                    if command == 'yes':
+                        print('you have accepted the file\n')
+                    else:
+                        print('you have rejected')
 
     def get_client_list(self):
         self.send(pickle.dumps({"command": "get_clients"}))
-        self.call("Hossein")
-        Timer(0.1, self.get_client_list).start()
+        #Timer(1, self.get_client_list).start()
 
-    def call(self, other_client):
-        self.send(pickle.dumps({"command": "call", "callee": other_client}))
-
-
+    def send_request(self, other_client):
+        self.send(pickle.dumps({"command": "send_file", "receiver": other_client}))
+        self.request_sent = True
+        #self.wait_for_answer()
 if __name__ == "__main__":
     CLIENT = ChatClient()
     CLIENT.run()
--- chat_server.py
+++ chat_server.py
@@ -17,6 +17,7 @@ class ChatServer:
         self.socket.bind(self.ADDR)
         self.socket.listen(5)
         self.ADDR = self.socket.getsockname()
+        self.requests = []
         print("Server woke up! Let's wait for customers on " + str(self.ADDR))
 
     def accept_incoming_connections(self):
@@ -30,33 +31,49 @@ class ChatServer:
 
     def handle_client(self, client):
         """Handles a single client connection."""
-
         name = client.recv(self.BUFFSIZE).decode("utf8")
         welcome = 'Welcome %s!' % name
         client.send(bytes(welcome, "utf8"))
         msg = "%s has joined the chat!" % name
         print(msg)
         self.client_names[name] = self.clients.index(client)
-
         while True:
+            #print('waiting for command from client' , name)
             msg = pickle.loads(client.recv(self.BUFFSIZE))
             if type(msg) is dict:
                 if msg['command'] == "get_clients":
                     self.send_client_list(client)
-                elif msg['command'] == "quit":
+                elif msg['command'] == "disconnect":
                     # FIXME: when user exits this should happen!
-                    client.send(bytes("{quit}", "utf8"))
+                    #client.send(bytes("{quit}", "utf8")) => why?!
+                    #del self.clients[client] => error : list indices must be integers or slices, not socket
+                    del self.clients[self.client_names[name]]
+                    del self.client_names[name] #delete username and indice
                     client.close()
-                    del self.clients[client]
                     print("%s has left the chat." % name)
                     break
-                elif msg['command'] == "call":
-                    print(self.client_names[msg['callee']])
+                elif msg['command'] == "send_file":
+                    print(name, 'wants send a file to', msg['receiver'])
+                    self.requests.append({'sender' : name, 'receiver' : msg['receiver'], 'status' : 'pending'})
+                    receiver_index = self.client_names[msg['receiver']]
+                    if(name != msg['receiver']): #you can not send a file to yourself
+                        self.send_file_request(name, msg['receiver'])
+                    #print(self.client_names[msg['callee']])
+                elif msg['command'] == 'answer':
+                    sender = self.clients[self.client_names[msg['sender']]]
+                    if(msg['value'] == 'yes'):
+                        self.send(name+'has accepted the file', sender)
+                    else:
+                        self.send(name+'has rejected the file', sender)
             else:
                 print(name + ": " + msg)
 
+    def send_file_request(self, sender, receiver):
+        receiver_index = self.client_names[receiver]
+        msg = pickle.dumps({"command": "send_file", "sender": sender})
+        self.send(msg, self.clients[receiver_index])
     def send_client_list(self, sock):
-        sock.sendall(pickle.dumps(tuple(self.client_names.values())))
+        sock.sendall(pickle.dumps(tuple(self.client_names.keys())))
 
     def broadcast(self, msg, prefix=""):  # prefix is for name identification.
         """Broadcasts a message to all the clients."""
@@ -73,6 +90,15 @@ class ChatServer:
         accept_thread.join()
         self.socket.close()
 
+    def send(self, msg, client):  # event is passed by binders.
+        """Handles sending of messages."""
+        if type(msg) is not bytes:
+            client.send(bytes(msg, "utf8"))
+        else:
+            client.send(msg)
+            #if msg == pickle.dumps({"command": "quite"}):
+                #self.client_socket.close()
+                #print("msg sent")
 
 if __name__ == "__main__":
     SERVER = ChatServer()
